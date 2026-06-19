7bea547f17b2e6034656712a5d2c7b6ceb06d684
jojoaja11 <jonathan@itbss.ac.id>
2026-06-19 19:57:19 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,40 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KRSController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\DosenController;
-use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\KelasController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Dashboard');
+    return view('dashboard', [
+        'user' => Auth::user(),
+    ]);
+})->name('dashboard');
+
+Route::middleware('auth')->group(function(){
+    Route::resource('/mahasiswa', MahasiswaController::class);
+    Route::resource('/dosen', DosenController::class);
+    Route::resource('/matakuliah', MatakuliahController::class);
+    Route::resource('/jurusan', JurusanController::class);
+    Route::resource('/kelas', KelasController::class);
+    Route::resource('/krs', KRSController::class);
 });
 
-Route::resource('/mahasiswa', MahasiswaController::class);
-Route::resource('/dosen', DosenController::class);
-Route::resource('/jurusan', JurusanController::class);
-Route::resource('/mata_kuliah', MatakuliahController::class);
-Route::resource('kelas', KelasController::class)
-Route::resource('/krs', KRSController::class);
-    ->except(['show', 'edit', 'update']);
+Route::get('/register', [AuthController::class, 'registerView']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::get('/login', [AuthController::class, 'loginView']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// Route::get      => Get Data     => R => select
+// SELECT ALL   /   SELECT SPESIFIK
+// Route::post     => Save Data    => C => insert into  /   create
+// Route::put      => Update Data  => U => update  /   alter
+// Route::delete   => Delete Data  => D => delete  /   drop
+
+// Create, Read, Update, Delete
